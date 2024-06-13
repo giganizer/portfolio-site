d330b1055f686936e5c566aa651f21ca85e70438
giganizer <49638783+giganizer@users.noreply.github.com>
2024-06-13 22:16:10 +0000

    Update About page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am an undergraduate Computer Science student from Slovenia. My biggest interests are artificial intelligence, machine learning and data science.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Programming",
+  "Machine learning",
+  "Python",
+  "Java",
+  "Go",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I've been in to computers for a long time. I like solving problems and creating things. I also love motorbikes and cars and anything petrol, including racing.";
 
 const About = () => {
   return (
